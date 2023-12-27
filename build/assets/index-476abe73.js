c5d318770debd2824dacfd066f29215dcb0a47f5
awqsomee <awqsome@bk.ru>
2023-12-27 12:03:33 +0300

    release: 27 12 23

---
+++ build/assets/index-476abe73.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-f1fdae2f.js";import{N as t}from"./index-5b8f0c27.js";import"./index-9240f5bc.js";const a=()=>o.jsx(t,{});export{a as default};
---
+++ build/assets/index-b8a1bbec.js
@@ -0,0 +1,10 @@
+import{y as l,s as p,o as d,bZ as g,j as e}from"./vendor-f1fdae2f.js";import{aV as m,V as x,W as P,w as j,m as k,k as v}from"./index-9240f5bc.js";import{c as y,L as w}from"./index-372f8b25.js";import"./index-f0b2e6bd.js";const F=l(async n=>{const{search:o="",page:c,limit:r,filter:s}=n,{data:a}=await m(o,(s==null?void 0:s.id.toString())??"",c,r);return{results:a.items}}),u=y({getFx:F,limit:50}),L=p.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    height: calc(100vh - var(--header-height) - 20px);
+
+    @media (max-width: 1000px) {
+        height: calc(100vh - 125px);
+    }
+`,W=()=>{const{$isPending:n,$items:o}=u,c=d(n),r=g(),s=d(o),{data:{user:a}}=x.useUser(),i=r.params.filter??(a==null?void 0:a.group)??"",h=t=>(t==null?void 0:t.title)==="Все"||!(t!=null&&t.id)?null:`Группа: ${t==null?void 0:t.id} • Всего: ${(s==null?void 0:s.length)??0}`;return e.jsx(P,{load:function(){},loading:c,error:null,data:s,children:e.jsx(L,{children:e.jsx(j,{children:e.jsx(w,{searchPlaceholder:"Поиск студентов",paginationList:u,filterRequest:k,filterPlaceholder:"Группа",defaultFilter:i,filter:(a==null?void 0:a.user_status)==="stud"?i:void 0,customMask:v.groupMask,underSearchText:h})})})})};export{W as default};

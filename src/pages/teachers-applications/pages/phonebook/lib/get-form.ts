4600153604271c8f289b319ace15bc9ecfe0b5e7
awqsomee <awqsome@bk.ru>
2024-01-12 14:01:52 +0300

    update form: add checkboxes, update site

--- src/pages/teachers-applications/pages/phonebook/lib/get-form.ts
+++ src/pages/teachers-applications/pages/phonebook/lib/get-form.ts
@@ -2,42 +2,75 @@ import { ContactInfoActualization, UserApplication } from '@api/model'
 import { getFormattedSubDivisions } from '@features/applications/lib/get-subdivisions'
 import { getDefaultSubdivision } from '@pages/teachers-applications/lib/get-default-subdivision'
 import { IInputArea } from '@ui/input-area/model'
+
 const sites = [
     {
         id: '0',
-        title: 'Б. Семёновская, д. 38',
+        title: 'ул. Большая Семёновская, 38',
     },
     {
         id: '1',
-        title: '1-я Дубровская, д. 16а',
+        title: 'ул. Прянишникова, 2А',
     },
     {
         id: '2',
-        title: 'Павла Корчагина, д. 22',
+        title: 'ул. Автозаводская, 16',
     },
     {
         id: '3',
-        title: 'Автозаводская, д. 16',
+        title: 'ул. Павла Корчагина, 22',
     },
     {
         id: '4',
-        title: 'Лефортовский вал, д. 26',
+        title: 'ул. Михалковская, 7',
     },
     {
         id: '5',
-        title: 'Прянишникова, д. 2А',
+        title: 'ул. Садовая-Спасская, 6',
     },
     {
         id: '6',
-        title: 'Садовая-Спасская, д. 6',
+        title: 'ул. Малая Семеновская, 12',
     },
     {
         id: '7',
-        title: 'Михалковская, д. 7',
+        title: 'ул. Лефортовский вал, 26',
     },
     {
-        id: '7',
-        title: 'Михалковская, д. 7',
+        id: '8',
+        title: 'ул. 1-я Дубровская, 16а',
+    },
+    {
+        id: '9',
+        title: 'ул. 7-я Парковая, 9/26',
+    },
+    {
+        id: '10',
+        title: 'ул. 800-летия Москвы, 28 корп. 1',
+    },
+    {
+        id: '11',
+        title: 'ул. Михалковская, 7, корп. 3',
+    },
+    {
+        id: '12',
+        title: 'ул. Бориса Галушкина, 9',
+    },
+    {
+        id: '13',
+        title: 'ул. Павла Корчагина, 20А, корп. 3',
+    },
+    {
+        id: '14',
+        title: 'Рижский проезд, 15, корп. 1',
+    },
+    {
+        id: '15',
+        title: 'Рижский проезд, 15, корп. 2',
+    },
+    {
+        id: '16',
+        title: '1-й Балтийский переулок, 6/21, корп. 3',
     },
 ]
 
@@ -47,9 +80,9 @@ const getForm = (data: ContactInfoActualization, dataForm: UserApplication): IIn
         title: 'Актуализация контактных данных',
         data: [
             {
+                fieldName: 'guid_staff',
                 title: 'Подразделение/должность',
                 value: getDefaultSubdivision(subdivisions),
-                fieldName: 'guid_staff',
                 editable: true,
                 width: '100',
                 required: true,
@@ -59,7 +92,7 @@ const getForm = (data: ContactInfoActualization, dataForm: UserApplication): IIn
             },
             {
                 fieldName: 'site',
-                title: 'Площадка (надо обновить)',
+                title: 'Площадка',
                 type: 'select',
                 value: null,
                 items: sites,
@@ -75,15 +108,21 @@ const getForm = (data: ContactInfoActualization, dataForm: UserApplication): IIn
                 required: true,
             },
             {
+                fieldName: 'email_staff',
                 title: 'Рабочий email',
                 type: 'email',
                 value: data.email_staff,
-                fieldName: 'email_staff',
                 required: true,
             },
             {
-                title: 'Показывать рабочий email в справочнике',
-                fieldName: 'show_email',
+                title: 'Показывать рабочий email внутри Личного кабинета',
+                fieldName: 'show_email_inner',
+                type: 'checkbox',
+                value: true,
+            },
+            {
+                title: 'Показывать рабочий email на сайте',
+                fieldName: 'show_email_outer',
                 type: 'checkbox',
                 value: true,
             },
@@ -94,10 +133,15 @@ const getForm = (data: ContactInfoActualization, dataForm: UserApplication): IIn
                 fieldName: 'tel_mob_staff',
                 mask: true,
             },
-
             {
-                title: 'Показывать мобильный телефон в справочнике',
-                fieldName: 'show_email',
+                title: 'Показывать мобильный телефон внутри Личного кабинета',
+                fieldName: 'show_tel_mob_staff_inner',
+                type: 'checkbox',
+                value: true,
+            },
+            {
+                title: 'Показывать мобильный телефон на сайте',
+                fieldName: 'show_tel_mob_staff_outer',
                 type: 'checkbox',
                 value: true,
             },
@@ -108,23 +152,16 @@ const getForm = (data: ContactInfoActualization, dataForm: UserApplication): IIn
                 fieldName: 'tel_staff',
             },
             {
-                title: 'Будет видно только для руководителя:',
-                type: 'text-header',
-                fieldName: '',
-                value: 'Будет видно только для руководителя:',
+                title: 'Показывать служебный телефон внутри Личного кабинета',
+                fieldName: 'show_tel_staff_inner',
+                type: 'checkbox',
+                value: true,
             },
             {
-                title: 'Почта отдела',
-                type: 'email',
-                value: '',
-                fieldName: 'subdivision_email',
-            },
-            {
-                title: 'Телефон отдела',
-                mask: true,
-                value: '',
-                type: 'tel',
-                fieldName: 'subdivision_phone',
+                title: 'Показывать служебный телефон на сайте',
+                fieldName: 'show_tel_staff_outer',
+                type: 'checkbox',
+                value: true,
             },
         ],
     }

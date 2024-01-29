c8234d714326134c2e5b3712ae4abd0e70c70920
Konstantin Doloz <takeonfaith6@gmail.com>
2024-01-29 23:53:19 +0300

    console removed

--- src/entities/schedule/lib/get-current-day-schedule.ts
+++ src/entities/schedule/lib/get-current-day-schedule.ts
@@ -10,11 +10,6 @@ const getCurrentDaySubjects = (daySubjects: ISubject[] | null, fullWeekDay: Date
 
         const fullStartDate = getFullStartDate(startDate)
         const fullEndDate = getFullEndDate(endDate, startDate)
-        // console.log(
-        //     normalizedFullWeekDay.toLocaleDateString(),
-        //     fullStartDate.toLocaleDateString(),
-        //     normalizedFullWeekDay <= (fullEndDate ?? fullStartDate),
-        // )
 
         return normalizedFullWeekDay >= fullStartDate && normalizedFullWeekDay <= (fullEndDate ?? fullStartDate)
     })

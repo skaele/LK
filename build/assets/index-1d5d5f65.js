09340fb10649d76f7314dd36e1e6af241ddfb83a
awqsomee <awqsome@bk.ru>
2024-03-14 14:05:58 +0300

    release: 14 03 24

---
+++ build/assets/index-1d5d5f65.js
@@ -0,0 +1,10 @@
+import{y as l,s as p,o as d,b$ as g,j as e}from"./vendor-4355a046.js";import{b6 as x,a6 as m,W as P,P as j,x as v,w as y}from"./index-15c11f6c.js";import{c as $,L as k}from"./index-f1d4fcaf.js";import"./index-ec40a62a.js";const w=l(async n=>{const{search:o="",page:c,limit:r,filter:s}=n,{data:a}=await x(o,(s==null?void 0:s.id.toString())??"",c,r);return{results:a.items}}),u=$({getFx:w,limit:50}),F=p.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    height: calc(100vh - var(--header-height) - 20px);
+
+    @media (max-width: 1000px) {
+        height: calc(100vh - 125px);
+    }
+`,W=()=>{const{$isPending:n,$items:o}=u,c=d(n),r=g(),s=d(o),{data:{user:a}}=m.useUser(),i=r.params.filter??(a==null?void 0:a.group)??"",h=t=>(t==null?void 0:t.title)==="Все"||!(t!=null&&t.id)?null:`Группа: ${t==null?void 0:t.id} • Всего: ${(s==null?void 0:s.length)??0}`;return e.jsx(P,{load:function(){},loading:c,error:null,data:s,children:e.jsx(F,{children:e.jsx(j,{children:e.jsx(k,{searchPlaceholder:"Поиск студентов",paginationList:u,filterRequest:v,filterPlaceholder:"Группа",defaultFilter:i,filter:(a==null?void 0:a.user_status)==="stud"?i:void 0,customMask:y.groupMask,underSearchText:h})})})})};export{W as default};

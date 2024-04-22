c369603f02a4ad7837a1215f07b3c1ce399c2a93
awqsomee <awqsome@bk.ru>
2024-04-22 12:45:51 +0300

    feat: move state to URL

--- src/pages/phonebook/index.tsx
+++ src/pages/phonebook/index.tsx
@@ -2,19 +2,21 @@ import { Wrapper } from '@pages/pe-student/styled'
 import { getDivisions } from '@shared/api/teacher-api'
 import { Loader } from '@shared/ui/atoms/loader'
 import PageBlock from '@shared/ui/page-block'
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Subdivisions } from './subdivisions'
 import { Staff } from './staff'
 import { Grid } from './styled/grid'
 import GlobalSearch from '@shared/ui/global-search'
 import SearchWithHints from '@features/search-with-hints'
 import Flex from '@shared/ui/flex'
-import { paginationList } from '@entities/all-teachers'
+// import { paginationList } from '@entities/all-teachers'
 import { Hint } from '@shared/ui/search'
 import { useUnit } from 'effector-react'
 import { phonebookModel } from '@entities/phonebook'
 import useCurrentDevice from '@shared/lib/hooks/use-current-device'
 import { Subdivision } from '@shared/api/model/phonebook'
+import useQueryParams from '@shared/lib/hooks/use-query-params'
+import { useHistory } from 'react-router'
 
 const recursiveSubdivisionSearch = (subdivisions: Subdivision[], searchString: string): Subdivision | null => {
     let respsonse = subdivisions.find((subdivision) => subdivision.name === searchString)
@@ -31,21 +33,30 @@ const recursiveSubdivisionSearch = (subdivisions: Subdivision[], searchString: s
 }
 
 const Phonebook = () => {
-    const [subdivisionSearch, setSubdivisionSearch] = useState('')
-    const [filter, setFilter] = useState<Hint | null>({
-        id: subdivisionSearch,
-        value: subdivisionSearch,
-        title: subdivisionSearch,
-    })
+    const query = useQueryParams()
+    const subdivision = query.get('subdivision') || ''
+    // const fio = query.get('fio') || ''
+    const history = useHistory()
+    // const [filter, setFilter] = useState<Hint | null>({
+    //     id: subdivision,
+    //     value: subdivision,
+    //     title: subdivision,
+    // })
     const subdivisions = useUnit(phonebookModel.stores.$subdivisions)
 
-    const { load } = paginationList
-    const handleSearch = async (value: string) => {
-        await load({ filter, search: value })
-    }
+    // const { load } = paginationList
+    // const handleSearch = async (value: string) => {
+    //     await load({ filter, search: value })
+    // }
+
+    useEffect(() => {
+        if (subdivision && subdivisions?.find) {
+            onHintClick({ id: subdivision, value: subdivision, title: subdivision })
+        }
+    }, [subdivisions])
 
     const onHintClick = (hint: Hint | undefined) => {
-        setFilter(hint ?? null)
+        // setFilter(hint ?? null)
         const searched = (subdivisions && recursiveSubdivisionSearch(subdivisions, hint?.value ?? '')) || null
         phonebookModel.events.setChosenSubdivision(searched)
     }
@@ -59,8 +70,12 @@ const Phonebook = () => {
                     <Grid columns={isMobile ? '1fr' : '4fr 7fr'} rows="1fr" columnGap="20px">
                         <SearchWithHints
                             width="100%"
-                            value={subdivisionSearch}
-                            setValue={setSubdivisionSearch}
+                            value={subdivision}
+                            setValue={(value) => {
+                                history.push({
+                                    search: '?subdivision=' + value,
+                                })
+                            }}
                             onHintClick={onHintClick}
                             placeholder={'Структура'}
                             request={getDivisions}
@@ -69,7 +84,8 @@ const Phonebook = () => {
                             <GlobalSearch
                                 triggerSearchOn={[]}
                                 placeholder={'Поиск'}
-                                searchApi={handleSearch}
+                                // searchApi={handleSearch}
+                                searchApi={async () => {}}
                                 validationCheck
                             />
                         )}

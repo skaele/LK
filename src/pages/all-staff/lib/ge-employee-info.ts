ba49555f7bd392a617504c1e4eed9a2ea1a023d5
awqsomee <awqsome@bk.ru>
2024-05-07 17:45:06 +0300

    feat: replace TeacherModal withStaffModal
    Allows staff to recieve extended info about staff. Students won't notice anything

---
+++ src/pages/all-staff/lib/ge-employee-info.ts
@@ -0,0 +1,23 @@
+import { Employee } from '@shared/api/model/phonebook'
+import { PhonebookInfo } from '../ui/phonebook-modal'
+
+export const getEmployeeInfo = (employee: Employee): PhonebookInfo[] =>
+    employee.job.map((job) => ({
+        subtitle: job.subdivision + ' • ' + job.post,
+        attributes: [
+            {
+                id: 'jobType',
+                title: 'Тип работы',
+                text: job.jobType,
+            },
+            {
+                id: 'email',
+                title: 'Корпоративная электронная почта',
+                text: job.email,
+            },
+            { id: 'innerPhone', title: 'Внутренний телефон', text: job.phone_inner },
+            { id: 'mobile', title: 'Служебный мобильный телефон', text: job.phone_direct },
+            { title: 'Адрес рабочего места', text: job.address },
+            { title: 'Номер кабинета', text: job.room },
+        ],
+    }))

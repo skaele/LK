192b12171f04365217ad8a6f3974745f5b463ccf
awqsomee <awqsome@bk.ru>
2024-06-26 20:53:57 +0300

    fix: update url

--- src/shared/api/config/tutorial-config.ts
+++ src/shared/api/config/tutorial-config.ts
@@ -2,8 +2,7 @@ import axios from 'axios'
 import { addAuthHeaderToRequests } from './utils'
 
 // export const TUTORIAL_URL = 'http://localhost:8000/tutorial/api'
-export const TUTORIAL_URL = 'http://apilin.mospolytech.ru:5070/tutorial/api'
-// http://apilin.mospolytech.ru:5070/tutorial/api/users/users
+export const TUTORIAL_URL = 'https://api.mospolytech.ru/tutorial/api'
 
 export const $tutorialApi = axios.create({ baseURL: TUTORIAL_URL })
 

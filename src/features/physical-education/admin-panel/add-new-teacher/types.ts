d6eed8639b63e25a1878fc43dd51412665b2c9a9
pritexx <vladon_2004@mail.ru>
2024-06-30 16:33:03 +0300

    migrate PE to rest API

--- src/features/physical-education/admin-panel/add-new-teacher/types.ts
+++
@@ -1,7 +0,0 @@
-import { PeTeacherAddPermission } from '@entities/pe-teacher/types'
-
-export type AddTeacher = {
-    teacherGuid: string
-    fullName: string
-    permission?: PeTeacherAddPermission
-}

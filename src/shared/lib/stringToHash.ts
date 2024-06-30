17b8e7b78afaaf400990756b8dded146c3bc2d45
awqsomee <awqsome@bk.ru>
2024-06-30 16:20:25 +0300

    update hash func

--- src/shared/lib/stringToHash.ts
+++ src/shared/lib/stringToHash.ts
@@ -1,13 +1,20 @@
+/**
+ * Converts a string into a 32-bit hash using a simple hashing algorithm.
+ * This algorithm is not cryptographically secure and should not be used
+ * for security-related purposes.
+ *
+ * @param {string} string - The input string to be hashed.
+ * @returns {number} A 32-bit integer hash code representing the input string.
+ */
 export function stringToHash(string: string) {
-    let hash = 0
-
-    if (string.length == 0) return hash
-
-    for (let i = 0; i < string.length; i++) {
-        const char = string.charCodeAt(i)
-        hash = (hash << 5) - hash + char
-        hash = hash & hash
+    let hash = 0,
+        i,
+        chr
+    if (string.length === 0) return hash
+    for (i = 0; i < string.length; i++) {
+        chr = string.charCodeAt(i)
+        hash = (hash << 5) - hash + chr
+        hash |= 0 // Convert to 32bit integer
     }
-
     return hash
 }

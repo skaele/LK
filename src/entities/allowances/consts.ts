964a4d921df51b957eccf9bc97b2ad20973e48e3
awqsomee <awqsome@bk.ru>
2024-07-05 10:30:34 +0300

    fix: keys

--- src/entities/allowances/consts.ts
+++ src/entities/allowances/consts.ts
@@ -1,6 +1,6 @@
 export const allowanceConstants = {
     Approved: 'Одобрено',
     Unknown: 'На рассмотрении',
-    InProgress: 'На рассмотрении',
+    InProgress: 'В работе',
     Declined: 'Отклонено',
 } as const

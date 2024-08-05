cb4e5425bcfdd0bde437622427360e8f9dc3e617
awqsomee <awqsome@bk.ru>
2024-08-05 14:43:59 +0300

    fix: icon

--- src/shared/ui/forbidden/index.tsx
+++ src/shared/ui/forbidden/index.tsx
@@ -1,14 +1,14 @@
 import { HOME_ROUTE } from '@app/routes/general-routes'
 import { Button, Error } from '@shared/ui/atoms'
 import React from 'react'
-import { IoHome } from 'react-icons/io5'
+import { FiHome } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 export const Forbidden = ({ text }: { text: string }) => {
     return (
         <Error text={text}>
             <Link to={HOME_ROUTE}>
-                <Button text="На главную" icon={<IoHome />} />
+                <Button text="На главную" icon={<FiHome />} />
             </Link>
         </Error>
     )

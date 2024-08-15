316a8401c9ae1be2be2e9db37dddfe7335d22d8a
awqsomee <awqsome@bk.ru>
2024-08-15 13:07:18 +0300

    deploy(4004): fix color of expired message

--- src/entities/allowances/lib/get-status.ts
+++ src/entities/allowances/lib/get-status.ts
@@ -2,4 +2,12 @@ import { MessageType } from '@shared/ui/types'
 import { ApprovalStatus } from '../types'
 
 export const getStatusType = (status: ApprovalStatus): MessageType =>
-    status === 'Approved' ? 'success' : status === 'Declined' ? 'failure' : status === 'Unknown' ? 'hint' : 'alert'
+    status === 'Approved'
+        ? 'success'
+        : status === 'Declined'
+        ? 'failure'
+        : status === 'Expired'
+        ? 'failure'
+        : status === 'Unknown'
+        ? 'hint'
+        : 'alert'

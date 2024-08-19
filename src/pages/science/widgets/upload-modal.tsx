ad87376781ef13ecf6173f1ed600ed5ddd34ef15
awqsomee <awqsome@bk.ru>
2024-08-19 12:47:20 +0300

    fix: update mobile view

--- src/pages/science/widgets/upload-modal.tsx
+++ src/pages/science/widgets/upload-modal.tsx
@@ -1,4 +1,5 @@
 import { scienceModel } from '@entities/science'
+import useCurrentDevice from '@shared/lib/hooks/use-current-device'
 import { SubmitButton } from '@shared/ui/atoms'
 import FileInput from '@shared/ui/file-input'
 import Flex from '@shared/ui/flex'
@@ -8,9 +9,10 @@ import React, { useEffect } from 'react'
 import { useModal } from 'widgets'
 
 export const UploadModal = () => {
+    const { isMobile } = useCurrentDevice()
     return (
         <Flex d="column" gap="0.5rem" mw="calc(800px + 1rem)">
-            <Flex gap="1rem" ai="flex-start">
+            <Flex gap="1rem" ai="flex-start" d={isMobile ? 'column' : 'row'}>
                 <ScopusFileInput />
                 <WosFileInput />
             </Flex>
@@ -20,9 +22,10 @@ export const UploadModal = () => {
 }
 
 const ScopusFileInput = () => {
+    const { isMobile } = useCurrentDevice()
     const [scopusFiles, setScopusFiles] = useUnit([scienceModel.stores.scopusFile, scienceModel.events.setScopusFile])
     return (
-        <Flex d="column" w="50%" mw="400px">
+        <Flex d="column" w={isMobile ? '100%' : '50%'} mw={isMobile ? '100%' : '400px'}>
             <Title size={4} align="left">
                 Scopus
             </Title>
@@ -38,9 +41,10 @@ const ScopusFileInput = () => {
 }
 
 const WosFileInput = () => {
+    const { isMobile } = useCurrentDevice()
     const [wosFiles, setWosFiles] = useUnit([scienceModel.stores.wosFile, scienceModel.events.setWosFile])
     return (
-        <Flex d="column" w="50%">
+        <Flex d="column" w={isMobile ? '100%' : '50%'} mw={isMobile ? '100%' : '400px'}>
             <Title size={4} align="left">
                 WoS
             </Title>

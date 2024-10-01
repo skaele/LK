e6404e08929bb8be3bd0eba8a0bdfa8a5e57cd21
awqsomee <awqsome@bk.ru>
2024-10-01 09:32:33 +0300

    deploy: personnel orders

--- src/shared/api/config/personnel-orders-config.ts
+++ src/shared/api/config/personnel-orders-config.ts
@@ -1,6 +1,6 @@
 import { createSecureApi } from '.'
 
-export const API_1С_CACHE_URL = `https://apilin.mospolytech.ru:4049/1c-cache`
+export const API_1С_CACHE_URL = `https://api.mospolytech.ru/1c-cache`
 export const API_HR_URL = `https://api.mospolytech.ru/serviceforfrontpersonnelorders`
 
 export const $1CCacheApi = createSecureApi(API_1С_CACHE_URL)

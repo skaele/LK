e726940072125e4bbc2e4be3f86a99db376b41a2
Владимир <awqsome@bk.ru>
2024-10-02 14:57:28 +0300

    Feat/add prof increased scholarship cervice (#209)
    * feat: add form factories
    
    * feat: add increased scholarship service

---
+++ src/shared/effector/form/create-file-filed.ts
@@ -0,0 +1,14 @@
+import { createEvent, createStore, Unit } from 'effector'
+
+export function createFilesField({ reset }: { reset?: Unit<any> | Array<Unit<any>> } = {}) {
+    const setValue = createEvent<File[]>()
+    const $store = createStore<File[]>([]).on(setValue, (_, newValue) => newValue)
+    if (reset)
+        if (Array.isArray(reset)) $store.reset(reset)
+        else $store.reset(reset)
+
+    return {
+        value: $store,
+        setValue,
+    }
+}

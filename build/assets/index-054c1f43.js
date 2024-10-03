a3e01c9066fe3212875b6204b07df065e8d246d2
awqsomee <awqsome@bk.ru>
2024-10-03 12:02:54 +0300

    release: 03 10 24

---
+++ build/assets/index-054c1f43.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-48260ac4.js";import{aE as s}from"./index-0474be1f.js";const t=()=>r.jsx(s,{isTeachers:!0});export{t as default};
---
+++ build/assets/index-fa3b3170.js
@@ -0,0 +1,1 @@
+import{j as s}from"./vendor-48260ac4.js";import"./index-0474be1f.js";import{T as t}from"./index-b89a806f.js";const p=()=>s.jsx(t,{isTeachers:!1});export{p as default};

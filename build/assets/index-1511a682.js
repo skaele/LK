c7e9d40d03cfa071a5a276923ac18ea2e8cfa38d
Владимир <awqsome@bk.ru>
2024-11-11 15:27:40 +0300

    release: 11 11 24

---
+++ build/assets/index-1511a682.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-48260ac4.js";import{N as e}from"./index-bfaca5b5.js";import"./index-078ea5cc.js";const i=()=>t.jsx(e,{docsType:!0});export{i as default};
---
+++ build/assets/index-77885557.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-48260ac4.js";import{N as t}from"./index-bfaca5b5.js";import"./index-078ea5cc.js";const a=()=>o.jsx(t,{});export{a as default};

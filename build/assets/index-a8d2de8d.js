273eac537620bc7a686e2f1916a3cc89ee5f51ec
Владимир <awqsome@bk.ru>
2024-11-21 09:46:48 +0300

    release: 21 11 24

---
+++ build/assets/index-a8d2de8d.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-48260ac4.js";import{bT as l,cu as t}from"./index-b11f750d.js";import{T as c}from"./index-6db02f63.js";const h=()=>{const{data:{schedule:s,externalSchedule:r,view:a},loading:u}=l.useSchedule(),e=u?t.schedule:r??s??t.schedule;return o.jsx(c,{showDates:!0,events:e.week,view:a,startDate:e.semestr.startDate,endDate:e.semestr.endDate})};export{h as default};
---
+++ build/assets/index-ac757350.js
@@ -0,0 +1,1 @@
+import{j as s}from"./vendor-48260ac4.js";import"./index-b11f750d.js";import{T as t}from"./index-ef7fe8d0.js";const p=()=>s.jsx(t,{isTeachers:!1});export{p as default};

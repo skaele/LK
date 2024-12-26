c6d8f2476397ff445df4ed28f2e272a98ff1ac7c
Владимир <awqsome@bk.ru>
2024-12-26 15:03:51 +0300

    release: 26 12 24 bbl

--- vite.config.ts
+++ vite.config.ts
@@ -67,7 +67,6 @@ export default defineConfig((conf) => {
             svgr(),
             splitVendorChunkPlugin(),
             checker({ typescript: true, eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' } }),
-            reactVirtualized(),
         ],
         build: {
             outDir: 'build',
@@ -82,26 +81,3 @@ export default defineConfig((conf) => {
         },
     }
 })
-
-const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
-export function reactVirtualized(): Plugin {
-    return {
-        name: 'flat:react-virtualized',
-        // Note: we cannot use the `transform` hook here
-        //       because libraries are pre-bundled in vite directly,
-        //       plugins aren't able to hack that step currently.
-        //       so instead we manually edit the file in node_modules.
-        //       all we need is to find the timing before pre-bundling.
-        configResolved() {
-            const file = require
-                .resolve('react-virtualized')
-                .replace(
-                    path.join('dist', 'commonjs', 'index.js'),
-                    path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js'),
-                )
-            const code = fs.readFileSync(file, 'utf-8')
-            const modified = code.replace(WRONG_CODE, '')
-            fs.writeFileSync(file, modified)
-        },
-    }
-}

f7500b6063cb987eb8f2045f7afddcdcda0f81ec
Владимир <awqsome@bk.ru>
2025-01-22 13:36:12 +0300

    release: 22 01 25

---
+++ build/assets/index-218b808b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-3c67dcca.js";import"./index-28ef9356.js";import{T as t}from"./index-05eb4351.js";import"./consts-e6e5e9f0.js";const a=()=>r.jsx(t,{isTeachers:!0});export{a as default};
---
+++ build/assets/index-61f9fd0f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-3c67dcca.js";import{N as t}from"./index-3b7f0f33.js";import"./index-28ef9356.js";import"./dev-mode-message-dfdef704.js";const e=()=>o.jsx(t,{});export{e as default};

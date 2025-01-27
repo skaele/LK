431f8acb7492ae37d1ae50a2c6bf2d23e4e262fc
Владимир <awqsome@bk.ru>
2025-01-27 14:51:24 +0300

    Feat/add student employment page (#243)

--- src/app/routes/other-routes/pages.ts
+++ src/app/routes/other-routes/pages.ts
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 export const ApplicationForSuperiorRoom = lazy(() => import('@pages/application-for-superior-room'))
 export const AcadPerformance = lazy(() => import('@pages/acad-performance'))
 export const DormitoryPage = lazy(() => import('@pages/dormitory'))
+export const StudentEmploymentPage = lazy(() => import('@pages/student-employment'))
 export const ClarificationOfPassportDataApplication = lazy(
     () => import('@pages/applications/pages/multifunctional-center/clarification-of-passport-data'),
 )
@@ -126,4 +127,4 @@ export const AccommodationCorrespondenceFormPage = lazy(
 export const FamilyRoomPage = lazy(() => import('@pages/applications/pages/campus-management/family-room'))
 
 export const ArbitraryRequestPage = lazy(() => import('@pages/applications/pages/other/arbitrary-request'))
-export const StudentEmploymentPage = lazy(() => import('@pages/applications/pages/other/student-employment'))
+export const StudentEmploymentApplicationPage = lazy(() => import('@pages/applications/pages/other/student-employment'))
